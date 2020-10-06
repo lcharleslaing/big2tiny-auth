c87133cdad2a8c12983bec9d00a7601eaa52506f
Lee Laing <lcharleslaing@gmail.com>
2020-10-06 13:42:46 -0400

    Updated Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Nuxt Strapi Auth</h1>
+      <h1 class="title">Welcome to Big2Tiny</h1>
+      <img src="~assets/icons/Icon1240x1240.png" width="200" ref=""/>
     </div>
   </section>
 </template>
